d960b5915f21395856693b4610c2920fe8643864
nareshgantala <nareshgantala@gmail.com>
2024-09-30 21:52:53 +0530

    robo

---
+++ catalogue.sh
@@ -0,0 +1,17 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+dnf install nodejs -y
+cp catalogue.service /etc/systemd/system/catalogue.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+useradd roboshop
+mkdir /app 
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
+cd /app 
+unzip /tmp/catalogue.zip
+cd /app 
+npm install 
+systemctl daemon-reload
+systemctl enable catalogue 
+systemctl start catalogue
+dnf install mongodb-org-shell -y
+mongo --host mongodb-dev.cloudlife.site </app/schema/catalogue.js
---
+++ user.sh
@@ -0,0 +1,14 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+dnf install nodejs -y
+cp user.service /etc/systemd/system/user.service
+useradd roboshop
+mkdir /app 
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip 
+cd /app 
+unzip /tmp/user.zip
+cd /app 
+npm install 
+systemctl daemon-reload
+systemctl enable user 
+systemctl start user
