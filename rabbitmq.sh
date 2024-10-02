c089fb48e2bb17e305c3092330ca61429e539edd
nareshgantala <nareshgantala@gmail.com>
2024-10-02 12:33:03 +0530

    rabbit

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,7 +1,20 @@
+echo -e "\e[32m<<<<<<<<<< congigure YUM repo  >>>>>>>\e[0m"
+
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+echo -e "\e[32m<<<<<<<<<< congigure YUM repo for rabbit MQ  >>>>>>>\e[0m"
+
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+
+echo -e "\e[32m<<<<<<<<<< Install rabbitmq server  >>>>>>>\e[0m"
+
 dnf install rabbitmq-server -y 
+
+echo -e "\e[32m<<<<<<<<<< restart rabbitmq server  >>>>>>>\e[0m"
+
 systemctl enable rabbitmq-server 
-systemctl start rabbitmq-server 
+systemctl restart rabbitmq-server 
+
+echo -e "\e[32m<<<<<<<<<< create roboshop user   >>>>>>>\e[0m"
+
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
