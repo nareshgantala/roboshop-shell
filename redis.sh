3ed5806fbc28360ae298fca815bcc79ee6567535
nareshgantala <nareshgantala@gmail.com>
2024-10-02 12:20:26 +0530

    pending

--- redis.sh
+++ redis.sh
@@ -1,7 +1,20 @@
+echo -e "\e[32m<<<<<<<<<< Install redis repo file  >>>>>>>\e[0m"
+
 dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+echo -e "\e[32m<<<<<<<<<< Enable redis 6.2 version  >>>>>>>\e[0m"
+
 dnf module enable redis:remi-6.2 -y
+
+echo -e "\e[32m<<<<<<<<<< Install redis  >>>>>>>\e[0m"
+
 dnf install redis -y 
+
+echo -e "\e[32m<<<<<<<<<< Replace 127.0.0.1 with 0.0.0.0 in redis.conf files  >>>>>>>\e[0m"
+
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
+
+echo -e "\e[32m<<<<<<<<<< Resatrt redis  >>>>>>>\e[0m"
+
 systemctl enable redis 
-systemctl start redis 
+systemctl restart redis 
--- shipping.sh
+++ shipping.sh
@@ -1,17 +1,43 @@
+echo -e "\e[32m<<<<<<<<<< Install maven  >>>>>>>\e[0m"
+
 dnf install maven -y
+
+echo -e "\e[32m<<<<<<<<<< add roboshop user  >>>>>>>\e[0m"
+
 useradd roboshop
+
+echo -e "\e[32m<<<<<<<<<< copy shipping service systemd file  >>>>>>>\e[0m"
+
 cp shipping.service /etc/systemd/system/shipping.service
 rm -rf /app
 mkdir /app 
+
+echo -e "\e[32m<<<<<<<<<< download shipping zip file  >>>>>>>\e[0m"
+
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip 
 cd /app 
+echo -e "\e[32m<<<<<<<<<< Unzip shipping files  >>>>>>>\e[0m"
+
 unzip /tmp/shipping.zip
 cd /app 
+echo -e "\e[32m<<<<<<<<<< Build application  >>>>>>>\e[0m"
+
 mvn clean package 
 mv target/shipping-1.0.jar shipping.jar 
+
+
+
+echo -e "\e[32m<<<<<<<<<< Install mysql  >>>>>>>\e[0m"
+
+dnf install mysql -y 
+
+echo -e "\e[32m<<<<<<<<<<Load schema, includes countries and cities  >>>>>>>\e[0m"
+
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/shipping.sql 
+
+
+echo -e "\e[32m<<<<<<<<<< Restart shippind service  >>>>>>>\e[0m"
+
 systemctl daemon-reload
 systemctl enable shipping 
-systemctl start shipping
-dnf install mysql -y 
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/shipping.sql 
 systemctl restart shipping
