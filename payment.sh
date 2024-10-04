ce53adbca70ae5b8cbe0061fa6870928ea1c00ba
nareshgantala <nareshgantala@gmail.com>
2024-10-04 15:57:10 +0530

    realpth

--- payment.sh
+++ payment.sh
@@ -1,4 +1,5 @@
-script_path=$(dirname $0)
+script=$(realpath "$0")
+script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
 echo -e "\e[32m<<<<<<<<<< Install python 3.6, and some dependencies  >>>>>>>\e[0m"
