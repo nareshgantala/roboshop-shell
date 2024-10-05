2eb05701768846766aa0f02fdef1bf5ea42255ab
nareshgantala <nareshgantala@gmail.com>
2024-10-05 11:57:34 +0530

    schema

--- common.sh
+++ common.sh
@@ -1,66 +1,98 @@
 app_user=roboshop
 
-print_head(){
+func_func_print_head(){
      echo -e "\e[33m<<<<<<<<<< $* >>>>>>>\e[0m"
 }
 
-schema_setup(){
+func_schema_setup(){
     if [ "$schema_setup" == mongo ]; then
 
-        print_head copy mongoDB repo file
+        func_print_head copy mongoDB repo file
         cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
 
-        print_head install mongodb shell 
+        func_print_head install mongodb shell 
 
         dnf install mongodb-org-shell -y
 
-        print_head Load master data of list of products 
+        func_print_head Load master data of list of products 
 
         mongo --host mongodb-dev.cloudlife.site </app/schema/${component}.js
     fi
+    if [ "$schema_setup" == mysql]; then
+        func_print_head Install mysql  
+        dnf install mysql -y 
+
+        func_print_headLoad schema, includes countries and cities  
+        mysql -h mysql-dev.cloudlife.site -uroot -p${mysql_root_password} < /app/schema/shipping.sql
+    fi 
+
 }
 
-
-func_nodejs(){
-    print_head "Disable current nodejs module"
-
-    dnf module disable nodejs -y
-
-    print_head "Enable 18 Version nodejs module"
-
-    dnf module enable nodejs:18 -y
-
-    print_head "Install nodejs" 
-
-    dnf install nodejs -y
-
-    print_head "Copy ${component} service systemd file" 
-
-    cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
-
-    print_head "Create roboshop user" 
-
+func_app_prereq(){
+    func_print_head add roboshop user  
     useradd ${app_user}
+    func_print_head create application directory
     rm -rf /app
     mkdir /app 
+    
+    func_print_head download application zip file  
 
-    print_head "Download ${component} zip file" 
     curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip 
     cd /app 
-
-    print_head "Unzip ${component} files" 
+    
+    func_print_head Unzip application files  
 
     unzip /tmp/${component}.zip
 
-    cd /app 
-    print_head "Install nodejs Dependencies" 
+}    
 
-    npm install 
+func_systemd_setup(){
 
-    print_head "Restart ${component} Service" 
+    func_print_head "Copy ${component} service systemd file" 
+    cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
 
+    func_print_head "Restart ${component} Service" 
     systemctl daemon-reload
     systemctl enable ${component} 
     systemctl restart ${component}
+    func_schema_setup
+
+}
+
+func_nodejs(){
+    func_print_head "Disable current nodejs module"
+    dnf module disable nodejs -y
+
+    func_print_head "Enable 18 Version nodejs module"
+    dnf module enable nodejs:18 -y
+
+    func_print_head "Install nodejs" 
+    dnf install nodejs -y
+    
+    func_app_prereq
+    
+    
+    func_print_head "Install nodejs Dependencies" 
+    npm install 
+
+    func_systemd_setup
+
+    func_schema_setup
+
+}
+
+func_java(){
+    func_print_head Install maven  
+    dnf install maven -y
+    
+    func_app_prereq
+
+    func_print_head Build application  
+    mvn clean package 
+    mv target/shipping-1.0.jar shipping.jar 
+
+
+
+    func_systemd_setup
 
 }
--- shipping.sh
+++ shipping.sh
@@ -7,46 +7,7 @@ if [ -z "mysql_root_password"]; then
     echo mysql password is missing
 fi
 
-echo -e "\e[32m<<<<<<<<<< Install maven  >>>>>>>\e[0m"
-
-dnf install maven -y
-
-echo -e "\e[32m<<<<<<<<<< add roboshop user  >>>>>>>\e[0m"
-
-useradd ${app_user}
-
-echo -e "\e[32m<<<<<<<<<< copy shipping service systemd file  >>>>>>>\e[0m"
-
-cp shipping.service /etc/systemd/system/shipping.service
-rm -rf /app
-mkdir /app 
-
-echo -e "\e[32m<<<<<<<<<< download shipping zip file  >>>>>>>\e[0m"
-
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip 
-cd /app 
-echo -e "\e[32m<<<<<<<<<< Unzip shipping files  >>>>>>>\e[0m"
-
-unzip /tmp/shipping.zip
-cd /app 
-echo -e "\e[32m<<<<<<<<<< Build application  >>>>>>>\e[0m"
-
-mvn clean package 
-mv target/shipping-1.0.jar shipping.jar 
-
-
-
-echo -e "\e[32m<<<<<<<<<< Install mysql  >>>>>>>\e[0m"
-
-dnf install mysql -y 
-
-echo -e "\e[32m<<<<<<<<<<Load schema, includes countries and cities  >>>>>>>\e[0m"
-
-mysql -h mysql-dev.cloudlife.site -uroot -p${mysql_root_password} < /app/schema/shipping.sql 
-
-
-echo -e "\e[32m<<<<<<<<<< Restart shippind service  >>>>>>>\e[0m"
-
-systemctl daemon-reload
-systemctl enable shipping 
-systemctl restart shipping
+component=shipping
+schema_setup=mysql
+func_java()
+func_systemd_setup()
